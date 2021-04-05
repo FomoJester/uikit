e82ee5484bab650a8dc48991babf1d13e6f508a2
dumplingswap <dumplingswap@outlook.com>
2021-04-05 11:07:45 +0100

    update exchange link

--- src/widgets/Menu/NavBar.tsx
+++ src/widgets/Menu/NavBar.tsx
@@ -15,12 +15,12 @@ const NavBar: React.FC<PanelProps> = ({ links }) => {
   return (
     <NavBody>
       <MenuEntry key={"/farms"} secondary isActive={false}>
-        <MenuLink href={"/farms"}>FARMS</MenuLink>
+        <MenuLink href={"https://dumplingswap.finance/farms"}>FARMS</MenuLink>
       </MenuEntry>
       <MenuEntry key={"/dough"} secondary isActive={false}>
-        <MenuLink href={"/dough"}>STAKING</MenuLink>
+        <MenuLink href={"https://dumplingswap.finance/dough"}>STAKING</MenuLink>
       </MenuEntry>
-      <MenuEntry key={"/exchange"} secondary isActive={false}>
+      <MenuEntry key={"https://exchange.dumplingswap.finance"} secondary isActive={false}>
         <MenuLink href={"/exchange"}>BUY DUMP</MenuLink>
       </MenuEntry>
     </NavBody>
